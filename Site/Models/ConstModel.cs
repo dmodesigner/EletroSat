0dc6aebb911f49eaaeb10434bea159058c391f85
Daniel Moura <daniel.dmo@live.com>
2023-08-23 23:51:09 -0300

    Adicionado variaveis a constante. Adicionado Exkyn Core ao View Import Global. Alterado o rodapé.

--- Site/Models/ConstModel.cs
+++ Site/Models/ConstModel.cs
@@ -5,6 +5,8 @@
 		public class Cliente
 		{
 			public const string Nome = "Eletro Sat";
-		}
+            public const string Telefone = "+55 (11) 99999-9999";
+            public const string Email = "contato@eletrosat.com.br";
+        }
 	}
 }
