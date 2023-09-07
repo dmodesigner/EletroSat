713d0f0d636b53e77ccd931be1383135a59913ef
Daniel Moura <daniel.dmo@live.com>
2023-09-07 14:18:21 -0300

    Criação das paginas de serviços: Principal Cabeamento de Rede Iluminação Projeto Eletrico Substituição de Cabeamento

---
+++ Site/Controllers/Servico.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site.Controllers
+{
+	public class Servico : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult CabeamentoRede()
+		{
+			return View();
+		}
+
+		public IActionResult Iluminacao()
+		{
+			return View();
+		}
+
+		public IActionResult ProjetoEletrico()
+		{
+			return View();
+		}
+		
+		public IActionResult SubstituicaoCabeamento()
+		{
+			return View();
+		}
+	}
+}
