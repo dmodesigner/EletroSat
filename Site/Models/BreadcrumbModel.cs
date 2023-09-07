5735380c6e4a6572ef05f16d0d613838865816b4
Daniel Moura <daniel.dmo@live.com>
2023-09-07 16:28:25 -0300

    Revisão: Correções relacionadas ao breadcrumb

--- Site/Models/BreadcrumbModel.cs
+++ Site/Models/BreadcrumbModel.cs
@@ -4,6 +4,7 @@
     {
         public string? Title { get; set; }
         public string? Page { get; set; }
+        public string? PageDefault { get; set; }
         public string? Image { get; set; }
         public string? ImageAlt { get; set; }
     }
