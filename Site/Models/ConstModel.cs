18fe9564829066d1b1de7ef3752f809d909a16ad
Daniel Moura <daniel.dmo@live.com>
2023-09-07 22:46:17 -0300

    Inclusão do e-mail do cliente. Inclusão das redes sociais. Correção do link do banner na home. Correção de palavras do banner. Otimizações de codigo para consentrar o nome do cliente apenas na variavel.

--- Site/Models/ConstModel.cs
+++ Site/Models/ConstModel.cs
@@ -6,7 +6,14 @@
 		{
 			public const string Name = "Eletro Sat";
             public const string Phone = "+55 (11) 93020-1545";
-            public const string Email = "contato@eletrosat.com.br";
+            public const string Email = "eletrosatservice@gmail.com";
         }
-	}
+
+        public class SocialMedia
+        {
+            public const string Facebook = "https://www.facebook.com/eletrosatservice/";
+			public const string Instagram = "https://www.instagram.com/eletrosatservice/";
+
+		}
+    }
 }
